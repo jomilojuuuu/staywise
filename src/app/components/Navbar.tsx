cb6ab23a13a683c69914b8d9302c8aed32c45435
jomiloju <jomilojuforlife2004@gmail.com>
2025-05-13 15:43:46 -0700

    i created the root of my applications✌

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function Navbar() {
+    return (
+        <div>
+            <div>
+                <h1>Booking.com</h1>
+             
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
