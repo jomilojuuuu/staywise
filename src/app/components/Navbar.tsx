0b0577f2877e5181d7b885e4eba1cef3e74c8764
jomiloju <jomilojuforlife2004@gmail.com>
2025-05-13 17:07:31 -0700

    working on the side bar ✌

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { FaRegUserCircle } from "react-icons/fa";
 
 
 function Navbar() {
-    return (
-        <div>
-            <div>
-                <h1>Booking.com</h1>
-             
-            </div>
+  return (
+    <div className=''>
+        <div className='flex justify-between items center px-4 mt-5'>
+            <h1 className='text-xl'>Booking.com</h1>
+            <FaRegUserCircle className='text-3xl'/>
         </div>
-    )
+    </div>
+  )
 }
 
 export default Navbar
