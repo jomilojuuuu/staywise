282dfd5144dd2e81c5040587a85186ec3a03e1eb
jomiloju <jomilojuforlife2004@gmail.com>
2025-05-14 10:23:25 -0700

    just finished the navbar✌

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,16 +1,48 @@
 import React from 'react'
+import Image from 'next/image';
+import { GoQuestion } from "react-icons/go";
 import { FaRegUserCircle } from "react-icons/fa";
+import { IoMdMenu } from "react-icons/io";
+
 
 
 function Navbar() {
-  return (
-    <div className=''>
-        <div className='flex justify-between items center px-4 mt-5'>
-            <h1 className='text-xl'>Booking.com</h1>
-            <FaRegUserCircle className='text-3xl'/>
+    return (
+        <div className='bg-blue-800 text-white font-bold'>
+            <div className='flex justify-between items-center px-4 md:px-4 mt-5'>
+                <h1 className='text-xl'>Booking.com</h1>
+                
+
+                {/* display on medium screen upward */}
+                <div className='w-[520px] hidden lg:flex gap-2 justify-between items-center'>
+                    <h1 className='p-3 hover:bg-blue-900 hover:rounded'>NGN</h1>
+                    <div className='p-3 hover:bg-blue-900 hover:rounded'>
+
+                        <div className="relative w-6 h-6 ">
+                            <Image
+                                src="/images/UsaFlag.png"
+                                alt="Company Logo"
+                                fill
+                                className="object-cover rounded-3xl "
+                            />
+                        </div>
+                    </div>
+                    <div className='p-3 hover:bg-blue-900 hover:rounded'>
+
+                        <GoQuestion className='text-2xl ' />
+                    </div>
+                    <h1 className='p-3 hover:bg-blue-900 hover:rounded'>List your property</h1>
+                    <button className='p-2 rounded bg-white text-blue-500 hover:bg-gray-50'>Register</button>
+                    <button className='p-2 rounded bg-white text-blue-500 hover:bg-gray-50'>Sign in</button>
+                </div>
+                    {/* display on small screen alone */}
+                <div className='flex w-20 justify-between lg:hidden'>
+                    <FaRegUserCircle className='text-3xl' />
+                    <IoMdMenu className='text-3xl' />
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="lg:px-[240px] bg-blue-400">
       <Navbar />
     </div>
   );
